c1776d0d104fc3860324a03e309297fcf44f5a1c
Ammar Yousuf Abrahani <60477445+ammarabrahani@users.noreply.github.com>
2025-03-23 00:17:07 +0000

    Update db.py
    remove code

--- db.py
+++ db.py
@@ -11,12 +11,6 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# ✅ Database Credentials from .env
-DB_USER = os.getenv("DB_USER", "admin")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "scalabledatabase1")
-DB_HOST = os.getenv("DB_HOST", "database-1.c36e9dieab0x.us-west-2.rds.amazonaws.com")
-DB_NAME = os.getenv("DB_NAME", "database-1")  # ✅ Changed hyphen to underscore
-DB_PORT = os.getenv("DB_PORT", "3306")
 
 # ✅ Connection URL for SQLAlchemy
 DATABASE_URL = "mysql+mysqlconnector://root:ammar@localhost:3306/database_1"
